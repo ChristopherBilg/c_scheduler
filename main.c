7a11e630d89198105e94a6232b6ab73ce3012d6a
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2019-01-23 14:31:43 -0500

    No real changes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "config.h"
 #include "priority_queue.h"
+#include "fifo_queue.h"
 
 int main() {
   struct Configuration conf = read_config_file("CONFIG.ini");
@@ -12,6 +13,9 @@ int main() {
   push_to_priority_queue(&p_queue, current_time, "Simulation starting");
   push_to_priority_queue(&p_queue, 0, "Job 1 arrived");
   push_to_priority_queue(&p_queue, conf.FIN_TIME, "Simulation ending");
+
+  // init. fifo_queue for cpu_queue, disk1_queue, and disk2_queue
+  // Make event enum Event
 
   // Main running loop
   while (current_time < conf.FIN_TIME) {
